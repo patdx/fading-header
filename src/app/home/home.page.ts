f4c88d8b680dd8cff954ef7ba7e45418cad48eed
Patrick Miller <thepdm@gmail.com>
2018-09-23 11:42:44 +0900

    add transition effect

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from "@angular/core";
+import { Content } from "@ionic/angular";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  @ViewChild("header")
+  headerRef: ElementRef;
 
+  @ViewChild(Content)
+  content: Content;
+
+  titleOpacity = 0;
+  headerOpacity = 1;
+
+  async ngOnInit() {
+    const header = this.headerRef.nativeElement as HTMLElement;
+    const scroller = await this.content.getScrollElement();
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          // header is visible
+          this.titleOpacity = 0;
+          this.headerOpacity = 1;
+        } else {
+          // header is not visible
+          this.titleOpacity = 1;
+          this.headerOpacity = 0;
+        }
+      },
+      { root: scroller }
+    );
+    observer.observe(header);
+  }
 }
