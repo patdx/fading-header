747c3350a7e54ad01e7c97d5f38fd7207e7a8e55
Patrick Miller <thepdm@gmail.com>
2018-09-23 17:33:45 +0900

    change implementation to scroll listener for iOS support

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # fading-header
 
-Fade a header using IntersectionObserver in Ionic 4.
+Fade a header in Ionic 4.
+
+Originally tried using IntersectionObserver, but it does not have native support on iOS Safari. And the standard polyfill does not work well with shadow DOM either. Therefore, it makes the most sense to use scroll event.
+
+TODO: Make sure the scroll event is being used passively for maximum performance.
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild, OnInit } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 import { Content } from "@ionic/angular";
 
 @Component({
@@ -6,34 +6,30 @@ import { Content } from "@ionic/angular";
   templateUrl: "home.page.html",
   styleUrls: ["home.page.scss"]
 })
-export class HomePage implements OnInit {
-  @ViewChild("header")
-  headerRef: ElementRef;
-
+export class HomePage {
   @ViewChild(Content)
   content: Content;
 
   titleOpacity = 0;
   headerOpacity = 1;
 
-  async ngOnInit() {
-    const header = this.headerRef.nativeElement as HTMLElement;
-    const scroller = await this.content.getScrollElement();
+  // This is the approximate scroll point where the header is
+  // 70% covered. It's a lot of work to get
+  // the actually height of the header. Even in ngAfterViewInit() lifecycle
+  // event, it's height is 0. So we can't get the real height of the header object
+  // until much too late.
+  threshold = 50;
 
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          // header is visible
-          this.titleOpacity = 0;
-          this.headerOpacity = 1;
-        } else {
-          // header is not visible
-          this.titleOpacity = 1;
-          this.headerOpacity = 0;
-        }
-      },
-      { root: scroller }
-    );
-    observer.observe(header);
+  onScroll(event: CustomEvent) {
+    const scrollTop = event.detail.scrollTop as number;
+    if (scrollTop > this.threshold) {
+      // header is not visible
+      this.titleOpacity = 1;
+      this.headerOpacity = 0;
+    } else {
+      // header is visible
+      this.titleOpacity = 0;
+      this.headerOpacity = 1;
+    }
   }
 }
